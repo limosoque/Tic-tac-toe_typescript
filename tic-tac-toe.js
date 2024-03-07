78e5c353cb319f2afeb6a1163777f846a6ecd8ff
limosoque <106828150+limosoque@users.noreply.github.com>
2024-03-07 13:30:36 +0300

    filling of the cell with the necessary symbol at the user's click added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,54 @@
 "use strict";
-document.body.innerHTML += `<p>${"Tic-tac-toe"}</p>`;
+var FieldValue;
+(function (FieldValue) {
+    FieldValue[FieldValue["FirstPlayer"] = 1] = "FirstPlayer";
+    FieldValue[FieldValue["SecondPlayer"] = 0] = "SecondPlayer";
+    FieldValue[FieldValue["NeutralElement"] = -1] = "NeutralElement";
+})(FieldValue || (FieldValue = {}));
+var GameSign;
+(function (GameSign) {
+    GameSign["FirstPlayerSign"] = "X";
+    GameSign["SecondPlayerSign"] = "O";
+})(GameSign || (GameSign = {}));
 const playfield_size = 3;
 //Создаем игровое поле и инициализируем его нейтральным значением
-let playfield = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]];
+let playfield = [
+    [FieldValue.NeutralElement, FieldValue.NeutralElement, FieldValue.NeutralElement],
+    [FieldValue.NeutralElement, FieldValue.NeutralElement, FieldValue.NeutralElement],
+    [FieldValue.NeutralElement, FieldValue.NeutralElement, FieldValue.NeutralElement]
+];
 const playfield_view = document.getElementById('playfield');
+//Определяем, кто ходит первым
+let current_player = Math.random() < 0.5 ? FieldValue.FirstPlayer : FieldValue.SecondPlayer;
 function InitializePlayfield() {
     //создание клеток игрового поля
-    for (let i = 0; i < playfield_size * playfield_size; i++) {
-        const cell = document.createElement('div');
-        cell.className = 'cell';
-        cell.addEventListener('click', () => OnCellClick());
-        playfield_view === null || playfield_view === void 0 ? void 0 : playfield_view.appendChild(cell);
+    for (let i = 0; i < playfield.length; i++) {
+        for (let j = 0; j < playfield[i].length; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.addEventListener('click', () => OnCellClick(i, j));
+            playfield_view.appendChild(cell);
+        }
     }
 }
-function OnCellClick() {
+function OnCellClick(row, column) {
+    if (playfield[row][column] == FieldValue.NeutralElement) {
+        playfield[row][column] = current_player;
+        ShowPlayfield();
+        //проверить победу или ничью
+    }
+}
+function ShowPlayfield() {
+    const cells = Array.from(playfield_view.children);
+    //ТУТ МОГУТ БЫТЬ ПРОБЛЕМЫ С КОНВЕРТАЦИЕЙ ИНДЕКСА
+    for (let i = 0; i < cells.length; i++) {
+        //Рассчитываем 2 индекса и 1
+        let row = Math.floor(i / playfield_size); //возможно, это не равно делению нацело
+        let column = i % playfield_size;
+        if (playfield[row][column] != FieldValue.NeutralElement) {
+            //Записываем в ячейку нужный символ
+            cells[i].textContent = playfield[row][column] == FieldValue.FirstPlayer ? GameSign.FirstPlayerSign : GameSign.SecondPlayerSign;
+        }
+    }
 }
 InitializePlayfield();
--- tic-tac-toe.ts
+++ tic-tac-toe.ts
@@ -1,23 +1,58 @@
-document.body.innerHTML += `<p>${"Tic-tac-toe"}</p>`;
+enum FieldValue {
+    FirstPlayer = 1,
+    SecondPlayer = 0,
+    NeutralElement = -1
+}
+
+enum GameSign {
+    FirstPlayerSign = 'X',
+    SecondPlayerSign = 'O'
+}
 
 const playfield_size: number = 3;
 //Создаем игровое поле и инициализируем его нейтральным значением
-let playfield: number[][] = [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]];
+let playfield: FieldValue[][] = [
+    [FieldValue.NeutralElement, FieldValue.NeutralElement, FieldValue.NeutralElement],
+    [FieldValue.NeutralElement, FieldValue.NeutralElement, FieldValue.NeutralElement],
+    [FieldValue.NeutralElement, FieldValue.NeutralElement, FieldValue.NeutralElement]];
 
-const playfield_view = document.getElementById('playfield');
+const playfield_view = document.getElementById('playfield')!;
+//Определяем, кто ходит первым
+let current_player: FieldValue = Math.random() < 0.5 ? FieldValue.FirstPlayer : FieldValue.SecondPlayer;
 
-function InitializePlayfield(){
+function InitializePlayfield() {
     //создание клеток игрового поля
-    for(let i = 0; i < playfield_size * playfield_size; i++){
-        const cell = document.createElement('div');
-        cell.className = 'cell';
-        cell.addEventListener('click', () => OnCellClick());
-        playfield_view?.appendChild(cell);
+    for (let i = 0; i < playfield.length; i++) {
+        for (let j = 0; j < playfield[i].length; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.addEventListener('click', () => OnCellClick(i, j));
+            playfield_view.appendChild(cell);
+        }
     }
 }
 
-function OnCellClick(){
+function OnCellClick(row: number, column: number) {
+    if (playfield[row][column] == FieldValue.NeutralElement) {
+        playfield[row][column] = current_player;
+        ShowPlayfield();
+        //проверить победу или ничью
+    }
+}
 
+function ShowPlayfield() {
+    const cells = Array.from(playfield_view.children);
+    //ТУТ МОГУТ БЫТЬ ПРОБЛЕМЫ С КОНВЕРТАЦИЕЙ ИНДЕКСА
+    for (let i = 0; i < cells.length; i++) {
+        //Рассчитываем 2 индекса и 1
+        let row: number = Math.floor(i / playfield_size); //возможно, это не равно делению нацело
+        let column: number = i % playfield_size;
+
+        if (playfield[row][column] != FieldValue.NeutralElement) {
+            //Записываем в ячейку нужный символ
+            cells[i].textContent = playfield[row][column] == FieldValue.FirstPlayer ? GameSign.FirstPlayerSign : GameSign.SecondPlayerSign;
+        }
+    }
 }
 
 

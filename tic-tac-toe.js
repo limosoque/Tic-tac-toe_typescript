90c222538fa8b9b63cb454262452768813a1f964
limosoque <106828150+limosoque@users.noreply.github.com>
2024-03-07 15:54:56 +0300

    2 players game and restart game added

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,6 +10,13 @@ var GameSign;
     GameSign["FirstPlayerSign"] = "X";
     GameSign["SecondPlayerSign"] = "O";
 })(GameSign || (GameSign = {}));
+var GameStatus;
+(function (GameStatus) {
+    GameStatus[GameStatus["InProcess"] = 0] = "InProcess";
+    GameStatus[GameStatus["FirstPlayerWin"] = 1] = "FirstPlayerWin";
+    GameStatus[GameStatus["SecondPlayerWin"] = 2] = "SecondPlayerWin";
+    GameStatus[GameStatus["Draw"] = 3] = "Draw";
+})(GameStatus || (GameStatus = {}));
 const playfield_size = 3;
 //Создаем игровое поле и инициализируем его нейтральным значением
 let playfield = [
@@ -35,15 +42,25 @@ function OnCellClick(row, column) {
     if (playfield[row][column] == FieldValue.NeutralElement) {
         playfield[row][column] = current_player;
         ShowPlayfield();
-        //проверить победу или ничью
+        let game_status = CheckGameStatus();
+        if (game_status == GameStatus.FirstPlayerWin || game_status == GameStatus.SecondPlayerWin) {
+            alert(`${current_player} wins!`);
+            RestartGame();
+        }
+        else if (game_status == GameStatus.Draw) {
+            alert(`Draw!`);
+            RestartGame();
+        }
+        else {
+            current_player = current_player == FieldValue.FirstPlayer ? FieldValue.SecondPlayer : FieldValue.FirstPlayer;
+        }
     }
 }
 function ShowPlayfield() {
     const cells = Array.from(playfield_view.children);
-    //ТУТ МОГУТ БЫТЬ ПРОБЛЕМЫ С КОНВЕРТАЦИЕЙ ИНДЕКСА
     for (let i = 0; i < cells.length; i++) {
-        //Рассчитываем 2 индекса и 1
-        let row = Math.floor(i / playfield_size); //возможно, это не равно делению нацело
+        //Рассчитываем 2 индекса из 1
+        let row = Math.floor(i / playfield_size);
         let column = i % playfield_size;
         if (playfield[row][column] != FieldValue.NeutralElement) {
             //Записываем в ячейку нужный символ
@@ -51,4 +68,88 @@ function ShowPlayfield() {
         }
     }
 }
+function CheckGameStatus() {
+    /*Проверка выигрыша*/
+    //Проверяем строки
+    for (let row = 0; row < playfield.length; row++) {
+        //Проверяем, что все элементы одной строки равны
+        if (playfield[row][0] != FieldValue.NeutralElement &&
+            playfield[row].every((value, _index, array) => value == array[0])) {
+            return playfield[row][0] == FieldValue.FirstPlayer ? GameStatus.FirstPlayerWin : GameStatus.SecondPlayerWin;
+        }
+    }
+    //Проверяем столбцы
+    for (let column = 0; column < playfield[0].length; column++) {
+        if (playfield[0][column] != FieldValue.NeutralElement) {
+            //Запоминаем первый элемент и проходимся по столбцу, смотря, есть ли другие FieldValue
+            let potential_winner = playfield[0][column];
+            for (let row = 1; row < playfield.length; row++) {
+                if (playfield[row][column] != potential_winner) {
+                    potential_winner = FieldValue.NeutralElement;
+                    break;
+                }
+            }
+            //Если в переменной остается один из игроков, значит он выиграл
+            if (potential_winner != FieldValue.NeutralElement) {
+                return potential_winner == FieldValue.FirstPlayer ? GameStatus.FirstPlayerWin : GameStatus.SecondPlayerWin;
+            }
+        }
+    }
+    //Проверяем левую диагональ
+    if (playfield[0][0] != FieldValue.NeutralElement) {
+        //Запоминаем первый элемент и проходимся по столбцу, смотря, есть ли другие FieldValue
+        let potential_winner = playfield[0][0];
+        for (let index = 1; index < playfield.length; index++) {
+            if (playfield[index][index] != potential_winner) {
+                potential_winner = FieldValue.NeutralElement;
+                break;
+            }
+        }
+        //Если в переменной остается один из игроков, значит он выиграл
+        if (potential_winner != FieldValue.NeutralElement) {
+            return potential_winner == FieldValue.FirstPlayer ? GameStatus.FirstPlayerWin : GameStatus.SecondPlayerWin;
+        }
+    }
+    //Проверяем правую диагональ
+    if (playfield[0][playfield.length - 1] != FieldValue.NeutralElement) {
+        //Запоминаем первый элемент и проходимся по столбцу, смотря, есть ли другие FieldValue
+        let potential_winner = playfield[0][playfield.length - 1];
+        for (let index = 1; index < playfield.length; index++) {
+            if (playfield[index][playfield.length - index - 1] != potential_winner) {
+                potential_winner = FieldValue.NeutralElement;
+                break;
+            }
+        }
+        //Если в переменной остается один из игроков, значит он выиграл
+        if (potential_winner != FieldValue.NeutralElement) {
+            return potential_winner == FieldValue.FirstPlayer ? GameStatus.FirstPlayerWin : GameStatus.SecondPlayerWin;
+        }
+    }
+    /*Проверка ничьей*/
+    let rows_draw = 0;
+    for (let row = 0; row < playfield.length; row++) {
+        if (playfield[row].every((value, _index, _array) => value != FieldValue.NeutralElement)) {
+            rows_draw++;
+        }
+    }
+    if (rows_draw == playfield.length) {
+        return GameStatus.Draw;
+    }
+    return GameStatus.InProcess;
+}
+function RestartGame() {
+    //Очищаем переменные
+    for (let row = 0; row < playfield.length; row++) {
+        for (let column = 0; column < playfield[row].length; column++) {
+            playfield[row][column] = FieldValue.NeutralElement;
+        }
+    }
+    current_player = Math.random() < 0.5 ? FieldValue.FirstPlayer : FieldValue.SecondPlayer;
+    //Очищаем клетки
+    const cells = Array.from(playfield_view.children);
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].textContent = '';
+    }
+}
+/*main*/
 InitializePlayfield();
